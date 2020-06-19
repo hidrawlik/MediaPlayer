4fc50270256e84df585a8825efd0d083d75f2a4e
Olexandr Slobodian <saschaslob@gmail.com>
2020-06-19 08:38:53 +0300

    Added authentication

--- MusicPlayer/MusicPlayer.API/Interface/IJwtTokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MusicPlayer.BLL.DTOs;
-
-namespace MusicPlayer.API.Interface
-{
-    public interface IJwtTokenService
-    {
-        string GenerateJwtToken(string email, UserDTO user);
-    }
-}
--- MusicPlayer/MusicPlayer.DAL/ConnectionString.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
-
-namespace MusicPlayer.DAL
-{
-    public static class ConnectionString
-    {
-        public static string Value { get; } = "Data Source=.\\SQLEXPRESS;Initial Catalog=MusicDB;Integrated Security=True";
-    }
-}
